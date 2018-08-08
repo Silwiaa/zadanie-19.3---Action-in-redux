5041602896ead0f51bddd5ef47ee762542acef84
Sylwia Krzymowska <s.krzymowska@gmail.com>
2018-08-08 18:28:58 +0200

    add new fiel

--- actions.js
+++ actions.js
@@ -1,1 +1,65 @@
 import uuid from uuid;
+import {ADD_COMMENT, REMOVE_COMMENT, EDIT_COMMENT, TUMB_UP_COMMENT, THUMB_DOWN_COMMENT} from ."./modulesActions"
+
+// ADD COMMENT
+function addComment(text) {
+    return {
+        type: ADD_COMMENT,
+        text,
+        id: uuid.v4()
+    }
+}
+
+const boundAddComment = text => dispatch(addComment(text));
+
+boundAddComment('Pierwszy nowy komentarz');
+boundAddComment('Kolejny nowy komentarz');
+
+// REMOVE COMMENT CREATOR
+function removeComment(id) {
+    return {
+        type: REMOVE_COMMENT,
+        id
+    }
+}
+
+const boundRemoveComment = id => dispatch(removeComment(id));
+
+boundRemoveComment(1);
+
+//EDIT COMMENT CREATOR
+function editComment(id, text) {
+    return {
+        type: EDIT_COMMENT,
+        id,
+        text
+    }
+}
+const boundEditComment = (id, text) => dispatch(editComment(id, text));
+
+boundEditComment('Pierwszy nowy komentarz', 1);
+
+//THUMB UP COMMENT CREATOR
+function thumbUpComment(thumb) {
+    return {
+        type: THUMB_UP_COMMENT,
+        thumb: thumb + 1
+    }
+}
+
+const boundThumbUpComment = id => dispatch(thumbUpComment(id));
+
+boundThumbUpComment(1);
+
+//THUMB DOWN COMMENT CREATOR
+function thumbDownComment(thumb, id) {
+    return {
+        type: THUMB_DOWN_COMMENT,
+        thumb: thumb - 1,
+        id
+    }
+}
+
+const boundThumbDownComment = id => dispatch(thumbDownComment(id));
+
+boundThumbDownComment(1);
